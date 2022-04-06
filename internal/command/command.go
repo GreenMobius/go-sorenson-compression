bc5fb725c312987bd9bc7a9fbb722d759f5be933
Taylor Chatfield <tchatfield16@gmail.com>
2022-04-05 22:15:23 -0400

    Move command processing to separate package

---
+++ internal/command/command.go
@@ -0,0 +1,61 @@
+package command
+
+import (
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var usage string = `Sorensen compression is a constant-size compression algorithm that solves all your compression needs.
+
+Usage:
+
+	sorensen <command> [flags] <file>
+
+The commands are:
+
+        compress  compress a file
+        extract   decompress a compressed file
+		info      view information on a compressed file
+
+Use "sorensen <command> -h" for more information about a command.`
+
+func RunCommand() {
+	compressCmd := flag.NewFlagSet("compress", flag.ExitOnError)
+	compressOutFile := compressCmd.String("o", "", "output file")
+
+	decompressCmd := flag.NewFlagSet("decompress", flag.ExitOnError)
+	decompressOutFile := decompressCmd.String("o", "", "output file")
+
+	flag.Parse()
+
+	args := os.Args
+	if len(args) < 3 {
+		log.Println(usage)
+		return
+	}
+
+	command := os.Args[1]
+	inputFile := os.Args[2]
+
+	switch command {
+	case "compress":
+		if *compressOutFile == "" {
+			*compressOutFile = strings.Trim(inputFile, filepath.Ext(inputFile)) + ".sor"
+		}
+		log.Println("Sorensen compression!")
+		log.Printf("  outfile: %s", *compressOutFile)
+	case "decompress":
+		if *decompressOutFile == "" {
+			*decompressOutFile = strings.Trim(inputFile, filepath.Ext(inputFile)) + ".sor"
+		}
+		log.Println("Sorensen decompression!")
+		log.Printf("  outfile: %s", *decompressOutFile)
+	case "info":
+		log.Println("Sorensen info!")
+	default:
+		log.Println(usage)
+	}
+}
